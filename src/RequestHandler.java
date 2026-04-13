6280f43199d71cea6bff7a7d3e6f24ca2a798d6f
rinesasekiraca2-create <rinesa.sekiraca2@student.uni-pr.edu>
2026-04-13 17:13:46 +0200

    Update RequestHandler
    Kontrollimi i gabimeve me bllokun Try

--- src/RequestHandler.java
+++ src/RequestHandler.java
@@ -6,4 +6,15 @@ import java.io.*;
 public class RequestHandler {
     private static final String SERVER_FOLDER = "server_files";
     private static final String ADMIN_IP = "192.168.1.105";
+
+    public static String handleRequest(String msg, InetAddress clientIP) {
+        try {
+            String[] parts = msg.split(" ", 3);
+            String command = parts[0];
+
+            boolean isAdmin = clientIP.getHostAddress().equals(ADMIN_IP);
+
+        
+        }
+    }
 }
